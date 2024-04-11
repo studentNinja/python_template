46226fdf2c58dddc1cbe89bc58186728b2c87d9e
studentNinja <v.galyts@ukma.edu.ua>
2024-04-11 22:20:17 +0300

    Input and output Docstrings

---
+++ app/io/input.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def input_from_console():
+    """
+    Function to input text from console.
+
+    Returns:
+        str: Text entered from the console.
+    """
+
+    pass
+
+
+def read_from_file(file_path):
+    """
+    Function to read from a file using Python's built-in capabilities.
+
+    Args:
+    -----
+        file_path (str): The path to the file to be read.
+
+    Returns:
+    --------
+        str: Content read from the file.
+            Returns None if file is not found.
+    """
+
+    pass
+
+
+def read_from_file_using_pandas(file_path):
+    """
+    Function to read from a file using pandas.
+
+    Args:
+    -----
+        file_path (str): Path to file .
+
+    Returns:
+    --------
+        pandas.DataFrame: Data read from the file using Pandas.
+            Returns None if file is not found.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,21 @@
+def output_to_console(text):
+    """
+    Function to output text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+    """
+
+    pass
+
+
+def write_to_file(file_path, content):
+    """
+    Function to write content to a file using Python's built-in capabilities.
+
+    Args:
+        file_path (str): The path to the file to be written.
+        content (str): The content to be written to the file.
+    """
+
+    pass
